50fc5cee2b17d4243a3b8aedc402848464000be2
Jason Calleiro <jason@theherostudio.com>
2008-11-10 09:32:38 -0500

    addedd partial for form, added new action view, added respond to methods in the links controller.

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,13 +1,23 @@
 class LinksController < ApplicationController
-  
+    
   # GET - Displays all links
   def index
     @links = Link.find :all, :order => 'created_at ASC'
+    respond_to do |format|
+      format.xml  { render :xml => @links.to_xml }
+      format.json { render :json => @links.to_json }
+      format.html { }
+    end
   end
 
   # GET - Shows one link (based on supplied id)
   def show
     @link = Link.find(params[:id])
+    respond_to do |format|
+      format.xml  { render :xml => @link.to_xml }
+      format.json { render :json => @link.to_json }
+      format.html { }
+    end
   end
 
   # GET - displays a form that can be used to create a link 
@@ -19,9 +29,17 @@ class LinksController < ApplicationController
   def create
     @link = Link.new(params[:post])
     @link.save!
-    redirect_to :action => link_path(@link)
+
+    respond_to do |format|
+      format.html { redirect_to :action => link_path(@link) }
+      format.xml  { render :xml => @link.to_xml, :status => :created }
+      format.json { render :json => @link.to_json, :status => :created }
+    end
     rescue ActiveRecord::RecordInvalid
-    render :action => "new"
+    respond_to do |format|
+      format.html { render :action => 'new' }
+      format.xml  { render :xml => @post.errors.to_xml, :status => 500 }
+    end
   end
 
   # GET - display a from allowing us to edit an existing link
@@ -34,8 +52,12 @@ class LinksController < ApplicationController
     @link = Link.find(params[:id])
     @link.attributes = params[:post]
     @link.save!
+    
+    respond_to do |format|
+      format.html { }
+    end
     redirect_to :action => link_path(@link)
-    rescue ActiveRecord::RecordInvalid
+  rescue ActiveRecord::RecordInvalid
     render :action => "edit"
   end
 
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,5 @@ module ApplicationHelper
   def title(page_title)
     content_for(:title) { page_title }
   end
+
 end
